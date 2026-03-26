20345db2bf0bc378c288f5d03856da82ce28769c
Laurent Stéphenne <laurent@guibi.dev>
2026-03-26 10:16:02 -0400

    Fixed typo

--- PiCar/control/picar/line.py
+++ PiCar/control/picar/line.py
@@ -4,15 +4,16 @@ from SunFounder.line_follower import Line_Follower
 
 
 class PiCarLine:
-    linefolower: Line_Follower
+    line_follower: Line_Follower
     line = [0, 0, 0, 0, 0]
 
     def __init__(self) -> None:
+        self.line_follower = Line_Follower()
         asyncio.create_task(self.read_line_task())
 
     async def read_line_task(self):
         await asyncio.sleep(2)
 
         while True:
-            self.distance = self.linefolower.read_analog()
+            self.distance = self.line_follower.read_analog()
             await asyncio.sleep(0.03)
