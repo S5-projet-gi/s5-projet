807f8cea7927e1e948fea411e7ff50da94c06052
Kgo20 <89485822+Kgo20@users.noreply.github.com>
2026-04-16 09:27:38 -0400

    Update const.py

--- PiCar/const.py
+++ PiCar/const.py
@@ -24,7 +24,7 @@ wall_avoidance = {
     # Reculer en tournant
     "trigger_distance": 15,
     "first_speed": -40,
-    "first_time": 35,
+    "first_time": 40,
     "first_angle": 40,
     # Avancer
     "second_speed": 40,
