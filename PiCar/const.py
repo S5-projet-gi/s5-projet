62a25f6b008596e3bd31ecce810fbb8552ec8382
Kgo20 <89485822+Kgo20@users.noreply.github.com>
2026-04-16 10:21:55 -0400

    Update const.py

--- PiCar/const.py
+++ PiCar/const.py
@@ -43,7 +43,7 @@ line_follower = {
     "black_threshold": 130,  # 130 on white
     "gray_threshold": 110,  # 110 on white
     "med_turn_angle": 20,
-    "med_turn_speed": 40,
+    "med_turn_speed": 35,
     "max_turn_angle": picar["max_turn_angle"],
     "max_turn_speed": 30,
     "lost_angle": picar["max_turn_angle"],
